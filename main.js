e1dad9f2e4094b1b933688d2c4c9b099131116e2
KaiserSov <sara310105@gmail.com>
2022-04-27 03:02:10 -0500

    Ball --- Aparece bolita para jugar. Falta organizar el choque con las bars.

--- main.js
+++ main.js
@@ -7,16 +7,39 @@
     this.game_over = false;
     this.bars = [];
     this.ball = null;
+    this.playing = false;
   }
 
   self.Board.prototype = {
     get elements(){
-      var elements = this.bars;
+      var elements = this.bars.map(function(bar){return bar;});
       elements.push(this.ball);
       return elements;
     }
   }
 })(); 
+
+(function(){
+  self.Ball = function(x,y,radius,board){
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.speed_x = 0;
+    this.speed_y = 3;
+    this.board = board;
+    this.direction = 1;
+
+    board.ball = this;
+    this.kind = "circle";
+  }
+  self.Ball.prototype = {
+    move: function(){
+      this.x += (this.speed_x * this.direction);
+      this.y += (this.speed_y);
+    }
+  }
+
+})();
 
 (function(){
   self.Bar = function(x,y,width,height,board){
@@ -27,7 +50,7 @@
     this.board = board;
     this.board.bars.push(this);
     this.kind = "rectangle";
-    this.speed = 10;
+    this.speed = 20;
   }
 
   self.Bar.prototype = {
@@ -56,45 +79,86 @@
   }
 
 self.BoardView.prototype = {
+  clean: function(){
+    this.contexto.clearRect(0,0,this.board.width,this.board.height);
+  },
+
   draw: function(){
     for (var i = this.board.elements.length - 1; i>=0; i--){
       var el = this.board.elements[i];
 
       draw(this.contexto,el);
     };
+  },
+  play: function(){
+   
+    if(this.board.playing){
+      this.clean();
+      this.draw();
+      this.board.Ball.move();
+    }
   }
 }
 
 function draw(contexto,element){
-  if(element !== null && element.hasOwnProperty("kind")){
-    switch(element.kind){
-      case "rectangle":
-        contexto.fillRect(element.x,element.y,element.width,element.height)
-        break;
-    }
+
+
+  switch(element.kind){
+     case "rectangle":
+      contexto.fillRect(element.x,element.y,element.width,element.height)
+    break;
+    case "circle":
+      contexto.beginPath();
+      contexto.arc(element.x,element.y,element.radius,0,7);
+      contexto.fill();
+      contexto.closePath();
+      break;
   }
 }
 
 })();
 
 var board = new Board(800,400);
-  var bar = new Bar(20,100,40,100,board);
-  var bar = new Bar(735,100,40,100,board);
-  var canvas = document.getElementById('canvas');
-  var board_view = new BoardView(canvas,board);
+var bar = new Bar(20,100,40,100,board);
+var bar_2 = new Bar(735,100,40,100,board);
+var canvas = document.getElementById('canvas');
+var board_view = new BoardView(canvas,board);
+var ball = new Ball(350, 100,10, board);
 
+
+
+//esto es para manejar las bars (ubicación)  
 document.addEventListener("keydown",function(ev){
+
   if(ev.keyCode == 38){
+    ev.preventDefault();
     bar.up();
   }
   else if(ev.keyCode == 40){
+    ev.preventDefault();
     bar.down();
+  }
+
+  if(ev.keyCode == 87){
+    ev.preventDefault();
+    bar_2.up();
+  }
+  else if(ev.keyCode == 83){
+    ev.preventDefault();
+    bar_2.down();
+  }
+  else if(ev.keyCode === 32){
+    ev.preventDefault();
+    board.playing = !board.playing;
   }
 });
 
-self.addEventListener("load",main);
+board_view.draw();
+
+window.requestAnimationFrame(controller);
 
 //control del cuadro para jugar Ping Pong
-function main(){
-  board_view.draw();
+function controller(){
+  board_view.play(); 
+  window.requestAnimationFrame(controller);
 }
