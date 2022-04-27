e881d89279c3fa6731937bf13cd1b28da8d99fa4
KaiserSov <sara310105@gmail.com>
2022-04-27 01:41:30 -0500

    Barras ---Se agregaron bars para jugar con sus propiedades

--- main.js
+++ main.js
@@ -12,11 +12,39 @@
   self.Board.prototype = {
     get elements(){
       var elements = this.bars;
-      elements.push(ball);
+      elements.push(this.ball);
       return elements;
     }
   }
 })(); 
+
+(function(){
+  self.Bar = function(x,y,width,height,board){
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.board = board;
+    this.board.bars.push(this);
+    this.kind = "rectangle";
+    this.speed = 10;
+  }
+
+  self.Bar.prototype = {
+    down: function(){
+      this.y += this.speed;
+    },
+
+    up: function(){
+      this.y -= this.speed;
+    },
+    toString: function(){
+      return "x: " + this.x + "y:" + this.y;
+    }
+  }
+
+  
+})();
 
 (function(){
   self.BoardView = function(canvas,board){
@@ -26,14 +54,47 @@
     this.board = board;
     this.contexto = canvas.getContext("2d");
   }
+
+self.BoardView.prototype = {
+  draw: function(){
+    for (var i = this.board.elements.length - 1; i>=0; i--){
+      var el = this.board.elements[i];
+
+      draw(this.contexto,el);
+    };
+  }
+}
+
+function draw(contexto,element){
+  if(element !== null && element.hasOwnProperty("kind")){
+    switch(element.kind){
+      case "rectangle":
+        contexto.fillRect(element.x,element.y,element.width,element.height)
+        break;
+    }
+  }
+}
+
 })();
 
+var board = new Board(800,400);
+  var bar = new Bar(20,100,40,100,board);
+  var bar = new Bar(735,100,40,100,board);
+  var canvas = document.getElementById('canvas');
+  var board_view = new BoardView(canvas,board);
+
+document.addEventListener("keydown",function(ev){
+  if(ev.keyCode == 38){
+    bar.up();
+  }
+  else if(ev.keyCode == 40){
+    bar.down();
+  }
+});
 
 self.addEventListener("load",main);
 
 //control del cuadro para jugar Ping Pong
 function main(){
-  var board = new Board(800,400);
-  var canvas = document.getElementById('canvas');
-  var board_view = new BoardView(canvas,board);
+  board_view.draw();
 }
